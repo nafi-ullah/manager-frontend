96f2c23d88f1a619673dd06bd920f20fa9b41c76
nafi-ullah <shafinnafiullah@gmail.com>
2024-05-05 14:06:38 +0600

    connection url updated

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,3 +1,5 @@
 // export const uri = 'http://localhost:8080';
-export const uri = 'http://54.236.250.225:8080';
+// export const uri = 'http://54.236.250.225:8080';
+export const uri = 'https://manager-backend-taupe.vercel.app';
+
 
